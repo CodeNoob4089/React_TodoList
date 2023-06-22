82254cdbb3c832909c9f73c3e0b61c6f291fd20f
bongho <wqoo2190@gmail.com>
2023-06-22 12:02:20 +0900

    css

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -15,34 +15,43 @@ function Todo({ todos, setTodos, isDone }) {
                 border: "1px solid black",
                 margin: "10px",
                 padding: "10px",
+                width: "200px",
+                backgroundColor: "#92b9f7",
               }}
             >
               <p>{todo.title}</p>
               <p>{todo.contents}</p>
               <p>{todo.id}</p>
               <p>{todo.isDone.toString()}</p>
-              <button
-                onClick={() => {
-                  const newTodos = todos.filter((Ft) => Ft.id !== todo.id);
-                  setTodos(newTodos);
+              <btngroup
+                style={{
+                  margin: "5px",
+                  padding: "5px",
                 }}
               >
-                삭제하기
-              </button>
-              <button
-                onClick={function () {
-                  const newTodos = todos.map(function (item) {
-                    if (item.id === todo.id) {
-                      return { ...item, isDone: !item.isDone };
-                    } else {
-                      return item;
-                    }
-                  });
-                  setTodos(newTodos);
-                }}
-              >
-                {isDone ? "취소" : "완료"}
-              </button>
+                <button
+                  onClick={() => {
+                    const newTodos = todos.filter((Ft) => Ft.id !== todo.id);
+                    setTodos(newTodos);
+                  }}
+                >
+                  삭제하기
+                </button>
+                <button
+                  onClick={function () {
+                    const newTodos = todos.map(function (item) {
+                      if (item.id === todo.id) {
+                        return { ...item, isDone: !item.isDone };
+                      } else {
+                        return item;
+                      }
+                    });
+                    setTodos(newTodos);
+                  }}
+                >
+                  {isDone ? "취소" : "완료"}
+                </button>
+              </btngroup>
             </div>
           );
         })}
